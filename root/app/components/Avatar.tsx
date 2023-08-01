2bc8235dc79d5c4da463115e52dbf90fc3d59953
Sanskar Tyagi <Sanskarbhartityagi@gmail.com>
2023-08-01 23:58:35 +0530

    ReCreated

--- root/app/components/Avatar.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import Image from 'next/image'
-import React from 'react'
-
-export default function Avatar() {
-  return (
-  <Image className='rounded-full ' height={30} width={30} alt='Avatar' src='/images/placeholder.jpg'/>
-  )
-}
--- root/app/page.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Home() {
-  return (
-<h1 className="text-rose-500 text-2xl">Hello BNB</h1>
-  )
-}
